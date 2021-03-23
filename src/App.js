df3f38fd21e1abd2d84b74dd24b5837a4a40e215
Lauren Ybarra <64549039+laurenybarra@users.noreply.github.com>
2021-03-22 20:13:46 -0700

    Added animated icon

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App(props) {
       city: response.data.name,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      icon: response.data.weather[0].icon
     });
   }
 
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormatDate from "./FormatDate";
 import WeatherTemp from "./WeatherTemp";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return (
@@ -8,12 +9,8 @@ export default function WeatherInfo(props) {
           <div class="weather-today col">
             <h1 class="city">{props.data.city}</h1>
             <h4 id="description" className="text-capitalize">{props.data.description}</h4>
-            <img
-              src={props.data.icon}
-              alt={props.data.description}
-              id="icon"
-            />
-            <WeatherTemp celsius={props.data.temperature} />
+            <WeatherIcon code={props.data.icon} />
+            <WeatherTemp celsius={props.data.temperature} />°
           </div>
           <div class="weather-conditions col">
             <h1 id="update">
@@ -22,7 +19,7 @@ export default function WeatherInfo(props) {
               <span class="date"></span>
             </h1>
             <h2 class="wind">
-              Wind: <span id="wind">{Math.round(props.data.wind)}</span>km/h
+              Wind: <span id="wind">{Math.round(props.data.wind)}</span> km/h
             </h2>
             <h2 class="humidity">
               Humidity: <span id="humidity">{Math.round(props.data.humidity)}</span>%
